e681b6a8a0836a112e596228df9ea723ee5a2bdf
odious_ophelia <odyeflynn602@mynccc.niagaracc.suny.edu>
2026-03-31 06:49:29 -0400

    feat: export core classes in __init__.py for simplified module imports

---
+++ core/__init__.py
@@ -0,0 +1,8 @@
+"""
+This file allows us to easily import our classes from the core folder.
+Think of it as the index at the back of a book.
+"""
+from .task import Task
+from .pet import Pet
+from .owner import Owner
+from .scheduler import Scheduler
